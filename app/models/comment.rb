8822f2f383a64e1a9f9bc139766ff8651587c9c6
spikeburton <ralphburton3@gmail.com>
2019-03-01 14:09:05 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(attr)
+    if !attr[:username].empty?
+      self.user = User.create(username: attr[:username])
+    end
+  end
+
 end
